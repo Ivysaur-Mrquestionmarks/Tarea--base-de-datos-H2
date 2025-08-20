db3151bf53aa9dd9cc060305065620c0d9db86c2
Ivysaur-Mrquestionmarks <75641314+Ivysaur-Mrquestionmarks@users.noreply.github.com>
2025-08-20 13:46:45 -0500

    set up

---
+++ nutrtion/src/main/java/com/eafit/nutrtion/NutrtionApplication.java
@@ -0,0 +1,13 @@
+package com.eafit.nutrtion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrtionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutrtionApplication.class, args);
+	}
+
+}
---
+++ nutrtion/src/test/java/com/eafit/nutrtion/NutrtionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eafit.nutrtion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutrtionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
